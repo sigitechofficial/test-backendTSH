bcc41908b6e020bf63a869fbfa795bfd1734810e
sigitechofficial <sigidevelopers@gmail.com>
2024-11-22 13:08:38 +0500

    add pm2 start feature in yml

--- trigger.php
+++ trigger.php
@@ -2,8 +2,11 @@
 // Path to your working directory
 $workingDir = '/home/theshippinghack/testing.theshippinghack.com';
 
-// Run npm install and pm2 start
-$output = shell_exec("cd $workingDir && npm install && pm2 start shipping.js 2>&1");
+// Run npm install
+$output = shell_exec("cd $workingDir && npm install 2>&1");
+
+// Run pm2 start using its full path
+$output .= shell_exec("cd $workingDir && ~/node/bin/pm2 start shipping.js 2>&1");
 
 // Output the result
 echo nl2br($output);
