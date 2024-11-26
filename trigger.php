639272502fe8de7bd4c496e2ff831b4a1136d921
sigitechofficial <sigidevelopers@gmail.com>
2024-11-26 16:06:36 +0500

    final yml version by ali

---
+++ trigger.php
@@ -0,0 +1,10 @@
+<?php
+// Path to your working directory
+$workingDir = '/home/theshippinghack/testing.theshippinghack.com';
+
+// Set HOME environment variable and run commands
+$output = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && npm install && ~/node/bin/pm2 start theshipping.js 2>&1");
+
+// Output the result
+echo nl2br($output);
+?>
