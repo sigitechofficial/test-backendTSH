fdb665913cd501c97f83c76a9798efb83e5c9323
sigitechofficial <sigidevelopers@gmail.com>
2024-11-26 17:14:19 +0500

    final yml version by ali

--- trigger.php
+++ trigger.php
@@ -4,11 +4,12 @@ $workingDir = '/home/theshippinghack/testing.theshippinghack.com';
 
 // Set HOME environment variable and run commands
 $commands = [
-    "export HOME=/home/theshippinghack", // Set the HOME environment
-    "cd $workingDir", // Navigate to the working directory
+    "export HOME=/home/theshippinghack", // Set the HOME variable to the correct directory
+    "cd $workingDir", // Change to the working directory
     "npm install", // Install dependencies
-    "~/node/bin/pm2 start theshipping.js || ~/node/bin/pm2 restart theshipping.js", // Start or restart the PM2 process
-    "~/node/bin/pm2 save" // Save the PM2 process list
+    "~/node/bin/pm2 start theshipping.js || ~/node/bin/pm2 restart theshipping.js", // Start or restart the application
+    "~/node/bin/pm2 save", // Save the PM2 process list
+    "~/node/bin/pm2 startup | grep 'sudo' | bash" // Ensure PM2 auto-starts on reboot
 ];
 
 $output = "";
