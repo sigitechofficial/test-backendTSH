5172fe72ef217af602004f89349185b94960057e
sigitechofficial <sigidevelopers@gmail.com>
2024-11-26 17:06:08 +0500

    final yml version by ali

--- trigger.php
+++ trigger.php
@@ -3,7 +3,19 @@
 $workingDir = '/home/theshippinghack/testing.theshippinghack.com';
 
 // Set HOME environment variable and run commands
-$output = shell_exec("export HOME=/home/theshippinghack && cd $workingDir && npm install && ~/node/bin/pm2 start theshipping.js 2>&1");
+$commands = [
+    "export HOME=/home/theshippinghack", // Set the HOME environment
+    "cd $workingDir", // Navigate to the working directory
+    "npm install", // Install dependencies
+    "~/node/bin/pm2 start theshipping.js || ~/node/bin/pm2 restart theshipping.js", // Start or restart the PM2 process
+    "~/node/bin/pm2 save" // Save the PM2 process list
+];
+
+$output = "";
+foreach ($commands as $command) {
+    $result = shell_exec($command . " 2>&1");
+    $output .= $result . "\n";
+}
 
 // Output the result
 echo nl2br($output);
